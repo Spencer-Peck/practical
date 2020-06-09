36db0f53784334aca858fcddbbd85180013faa27
Spencer Peck <pec15002@byui.edu>
2020-06-09 10:11:41 -0700

    added a navbar and a getGames cloud api

--- practical/functions/index.js
+++ practical/functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
+admin.initializeApp();
+
+let db = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.getGames = functions.https.onRequest((req, res) => {
+    db.collection("boardgames")
+    .get()
+    .then((data) => {
+        let games = [];
+        data.forEach((doc) => {
+            games.push(doc.data());
+        });
+        return res.json(games);
+    })
+    .catch((err) => console.errror(err));
+});
