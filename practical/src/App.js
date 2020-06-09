b80c7f5439e18f309258c222fa8221e9f053b9fa
Spencer Peck <pec15002@byui.edu>
2020-06-09 11:44:49 -0700

    added axios and a get request to get the game data

--- practical/src/App.js
+++ practical/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Component from 'react';
 import './App.css';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
@@ -9,6 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import axios from "axios";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -23,24 +25,71 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App() {
+function NavBar() {
 
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Board Game Score Keeper
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Board Game Score Keeper
         </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-  </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+
+}
+
+class BoardgameList extends React.Component {
+
+  state = {
+    games: null
+  }
+
+  componentDidMount(){
+    axios.get('https://us-central1-practical-4eb41.cloudfunctions.net/getGames')
+    .then(res => {
+      this.setState({
+        games: res.data
+      })
+    })
+  }
+
+
+  render() {
+    
+    //const classes = useStyles();
+    let gamesMarkup = this.state.games ? (
+      this.state.games.map(game =><p>{game.title}</p>)
+    ) : <p>Loading...</p>
+
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {gamesMarkup}
+        </Grid>
+  
+      </Grid>
+    );
+  }
+}
+
+function App() {
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <NavBar />
+      <BoardgameList />
+
+    </div>
   );
 }
 
