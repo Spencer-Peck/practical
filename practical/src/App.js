0850a05ec541a18a3ceba3e9f46302fb4d247bf8
Spencer Peck <pec15002@byui.edu>
2020-06-09 12:13:30 -0700

    Added a simple table to view the games list from the DB

--- practical/src/App.js
+++ practical/src/App.js
@@ -11,6 +11,13 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import axios from "axios";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -52,29 +59,47 @@ class BoardgameList extends React.Component {
     games: null
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://us-central1-practical-4eb41.cloudfunctions.net/getGames')
-    .then(res => {
-      this.setState({
-        games: res.data
+      .then(res => {
+        this.setState({
+          games: res.data
+        })
+        console.log(res.data);
       })
-    })
   }
 
 
   render() {
-    
+
     //const classes = useStyles();
     let gamesMarkup = this.state.games ? (
-      this.state.games.map(game =><p>{game.title}</p>)
+      this.state.games.map(game => <p>{game.title}</p>)
     ) : <p>Loading...</p>
 
     return (
       <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
-          {gamesMarkup}
+          <TableContainer component={Paper}>
+            <Table  aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Game</TableCell>
+                  <TableCell align="right">Image</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                  <TableRow >
+                    <TableCell component="th" scope="row">
+                      {gamesMarkup}
+                    </TableCell>
+                    <TableCell align="right"></TableCell>
+                  </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
-  
+
       </Grid>
     );
   }
